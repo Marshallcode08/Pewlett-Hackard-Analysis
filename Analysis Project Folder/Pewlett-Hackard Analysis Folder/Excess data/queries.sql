84a3f9478d86538a27c49e7f8998edd89e9ccce7
Marshallcode08 <112818881+Marshallcode08@users.noreply.github.com>
2022-10-31 20:26:21 -0400

    Add files via upload

---
+++ Analysis Project Folder/Pewlett-Hackard Analysis Folder/Excess data/queries.sql
@@ -0,0 +1,145 @@
+-- Creating tables for PH-EmployeeDB
+CREATE TABLE departments (
+     dept_no VARCHAR(4) NOT NULL,
+     dept_name VARCHAR(40) NOT NULL,
+     PRIMARY KEY (dept_no),
+     UNIQUE (dept_name)
+);
+
+-- Creating Tables for PH-EmployeesDB(Employees)
+CREATE TABLE employees (emp_no INT NOT NULL,
+     birth_date DATE NOT NULL,
+     first_name VARCHAR NOT NULL,
+     last_name VARCHAR NOT NULL,
+     gender VARCHAR NOT NULL,
+     hire_date DATE NOT NULL,
+     PRIMARY KEY (emp_no)
+);
+
+--Creating Tables for PH-EmployeesDB(Dept_manager)
+CREATE TABLE dept_manager (
+dept_no VARCHAR(4) NOT NULL,
+    emp_no INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+
+--Creating Tables for PH-Employees(Salaries)
+CREATE TABLE salaries (
+  emp_no INT NOT NULL,
+  salary INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no)
+);
+
+--Creating Tables for PH-EmployeesDB(Title)
+CREATE TABLE titles(
+	emp_no INT NOT NULL,
+	title varchar NOT NULL,
+	from_date Date NOT NULL, 
+	to_date Date NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
+	PRIMARY KEY (emp_no)
+);
+
+--Creating Tables for PH-EmployeesDB(Dept_emp)
+CREATE TABLE dept_emp(
+dept_no varchar NOT NULL,
+emp_no int NOT NULL,
+from_date Date NOT NULL,
+to_date Date NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments(dept_no),  
+PRIMARY KEY (emp_no, dept_no)
+);
+
+-- Testing the table 
+select * from departments;
+
+--Testing the table
+select * from employees;
+
+--Resting the table 
+select * from dept_manager;
+
+--Testing the table 
+select * from salaries;
+
+--Determine retirement eligibility 
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+--Check employees born in the 1952
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+--Create a new table retirenment_info
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+--Query on retirement_info
+SELECT * FROM retirement_info;
+
+--Drop retirement_info
+DROP TABLE retirement_info;
+
+-- Create new table for retiring employees
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- Check the table
+SELECT * FROM retirement_info;
+
+-- Joining departments and dept_manager tables
+SELECT departments.dept_name,
+     dept_manager.emp_no,
+     dept_manager.from_date,
+     dept_manager.to_date
+FROM departments
+INNER JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no;
+
+-- Joining retirement_info and dept_emp tables - check emp_no
+SELECT retirement_info.emp_no,
+    retirement_info.first_name,
+retirement_info.last_name,
+    dept_emp.to_date
+FROM retirement_info
+LEFT JOIN dept_emp
+ON retirement_info.emp_no = dept_emp.emp_no;
+
